66eabbc6bd5642f2f405b3aa9d64d59e29a3474b
CoffeCup12 <shaozhu19@gmail.com>
2024-05-18 21:03:43 -0400

    styling input page

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/planning")
+@app.route("/planning", methods = ["POST", "GET"])
 def interact():
-    return render_template("interact.html")
+    if request.method == "POST":
+        response = request.form["userInput"]
+        session["response"] = response  #stored user response in a session 
+        return redirect(url_for("interact")) #must have a return statement so I will just put it like this for now 
+    else:
+        return render_template("interact.html")
 
 @app.route("/about")
-def about():
+def about():    
     return render_template("about.html")
 
 if __name__ == "__main__":
