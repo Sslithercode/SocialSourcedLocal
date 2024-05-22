de943e881647975e6b6bc98ca1cd59a340fe4db8
CoffeCup12 <shaozhu19@gmail.com>
2024-05-21 23:08:05 -0400

    fixing interact styling

--- webapp/main.py
+++ webapp/main.py
@@ -3,6 +3,8 @@ from flask import Flask, redirect, url_for, render_template, request, session
 app = Flask(__name__)
 app.secret_key = "hello"
 
+messages = []
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -10,14 +12,23 @@ def home():
 @app.route("/planning", methods = ["POST", "GET"])
 def interact():
     if request.method == "POST":
-        response = request.form["userInput"]  
-        return render_template("interact.html", answer = response) #must have a return statement so I will just put it like this for now 
+        response = request.form["userInput"]
+        messages.append(response)
+        return render_template("interact.html", data = messages)
     else:
-        return render_template("interact.html", answer = "")
+        return render_template("interact.html", data = messages)
 
 @app.route("/about")
 def about():    
     return render_template("about.html")
 
+@app.route('/clean_list', methods=['POST'])
+def clean_list():
+    global messages
+    messages = []
+    return redirect(url_for("interact"))
+
+
 if __name__ == "__main__":
     app.run(debug = True)
+
